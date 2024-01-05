e8f1c74f358d42ae66f9bf1e33c68ed7de91e039
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-05 15:57:45 +0100

    Added a utility section which is meant to make the three letter thing work.
    three letters are how School ID's typically work.

--- MiniAPI/Handlers/Utility.cs
+++ MiniAPI/Handlers/Utility.cs
@@ -1,6 +1,24 @@
-﻿namespace MiniAPI.Handlers
+﻿using MiniAPI.Models;
+
+namespace MiniAPI.Handlers
 {
     public class Utility
     {
+        //First letters of the First and last names to create an ID
+        public static string BuildPersonId(string firstName, string lastName)
+        {
+            return string.Join(' ', firstName[0], firstName[1], firstName[2], lastName[0], lastName[1], lastName[2]).Trim();
+        }
+
+        //first 3 letters of the Interest to create an ID
+        public static string BuildInterestId(string title)
+        {
+            return string.Join(' ', title[0], title[1], title[2]).Trim();
+        }
+
+        public static string PullFirstLastName(Person person)
+        {
+            return $"{person.FirstName} {person.LastName}";
+        }
     }
 }
