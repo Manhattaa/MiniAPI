5ed69bebb49b235283432de5a73ba1571a07afef
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-09 23:03:20 +0100

    Patch Notes:
    Testing the waters with Controllers.

---
+++ MiniAPI/Models/Link2InterestsController.cs
@@ -0,0 +1,16 @@
+﻿namespace MiniAPI.Models
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MiniAPI.Models;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Link2InterestsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateLink([FromBody] Link2Interests link2Interest)
+        {
+            return Ok("Link created successfully");
+        }
+    }
+}
